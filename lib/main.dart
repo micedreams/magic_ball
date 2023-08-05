864b5e94cacbc9423fe8ff1ca7a55ce199c41912
Aku <micedreams@micedreams.domain.name>
2023-08-05 22:58:18 +0530

    Fix colors.

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xffFDBE50),
+          background: Colors.black,
+        ),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
@@ -31,9 +33,7 @@ final list = [
 ];
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    super.key,
-  });
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -57,29 +57,31 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Magic_ball'),
+        title: const Text('Magic ball'),
       ),
       body: Center(
-        child: Stack(
-          alignment: AlignmentDirectional.center,
-          children: [
-            Lottie.asset('assets/magic_ball.json'),
-            ValueListenableBuilder<int>(
-              builder: (context, value, _) {
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 50.0),
-                  child: InkWell(
-                    onTap: _randomValue,
+        child: InkWell(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          onTap: _randomValue,
+          child: Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Lottie.asset('assets/magic_ball.json'),
+              ValueListenableBuilder<int>(
+                builder: (context, value, _) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 80.0),
                     child: Text(
                       list[value],
-                      style: Theme.of(context).textTheme.headlineMedium,
+                      style: Theme.of(context).textTheme.labelSmall,
                     ),
-                  ),
-                );
-              },
-              valueListenable: indexNotifier,
-            ),
-          ],
+                  );
+                },
+                valueListenable: indexNotifier,
+              ),
+            ],
+          ),
         ),
       ),
     );
